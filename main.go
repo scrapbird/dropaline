d1c7abde6e22fd85dc293f92257ad833fc7a35cc
Brenton Morris <brenton.smorris@gmail.com>
2015-02-27 09:59:06 +1300

    First commit

---
+++ main.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	irc "github.com/fluffle/goirc/client"
+	"flag"
+	"fmt"
+)
+
+func main() {
+	// check flags
+	var serverAddress string
+	var port int
+	var targetNick string
+	var nick string
+	var message string
+
+	flag.StringVar(&serverAddress, "s", "", "The server to log in to")
+	flag.IntVar(&port, "p", 6667, "The port to connect on")
+	flag.StringVar(&targetNick, "t", "", "The user to send the message to")
+	flag.StringVar(&nick, "n", "dropaline", "The nick to log into the server with")
+	flag.StringVar(&message, "m", "Alert!", "The message to be sent to the target")
+	flag.Parse()
+
+	quit := make(chan bool)
+
+	// connect and send the message
+	ircConn := irc.SimpleClient(nick)
+
+	ircConn.HandleFunc("connected", func(conn *irc.Conn, line *irc.Line) {
+		conn.Privmsg(targetNick, message)
+		quit <- true
+	})
+	ircConn.HandleFunc("disconnected", func(conn *irc.Conn, line *irc.Line) {
+		quit <- true
+	})
+
+	err := ircConn.ConnectTo(serverAddress)
+	if err != nil {
+		fmt.Println("Error:" + err.Error())
+		return
+	}
+
+	<-quit
+	ircConn.Quit()
+}
+
